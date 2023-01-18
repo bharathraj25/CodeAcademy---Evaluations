85d1274b24d6acff7c4e31214b077838434fb554
Bharath Raj <BharathRajgngowdara@gmail.com>
2023-01-18 12:44:09 +0530

    feat: add getScoresByFrames to filter out the input by frames

--- evaluationOne/bowlingGameUtility.js
+++ evaluationOne/bowlingGameUtility.js
@@ -13,3 +13,29 @@ const getFrameStatus = frame => {
   }
   return 0
 }
+
+const getScoresByFrames = eachBallScores => {
+  let currentFrame = 1;
+  let currentTotal = 0;
+
+  const frameScore = { 1: [] }
+
+  eachBallScores.forEach(score => {
+    console.log(currentFrame == 1);
+    if (currentFrame != 10) {
+      if (
+        currentTotal == 10
+        ||
+        frameScore[currentFrame].length == 2
+      ) {
+        currentFrame += 1
+        currentTotal = 0
+        frameScore[currentFrame] = []
+      }
+    }
+    frameScore[currentFrame].push(score)
+    currentTotal += score
+  });
+
+  return frameScore;
+}
